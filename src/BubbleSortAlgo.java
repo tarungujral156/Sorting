de378a1289de8d0771a3d12395400b31fe9c742f
Tarun Gujral <tarungujral156@gmail.com>
2023-01-15 21:33:03 +0530

    add selectionSort

--- src/BubbleSortAlgo.java
+++ src/BubbleSortAlgo.java
@@ -9,13 +9,19 @@ public class BubbleSortAlgo {
             for (int j = 1; j <= arr.length - i - 1; j++) {
 
                 if(arr[j] < arr[j - 1]){
-                    int temp;
-                    temp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = temp;
+//                    int temp;
+//                    temp = arr[j];
+//                    arr[j] = arr[j - 1];
+//                    arr[j - 1] = temp;
+                    swap(arr, j, j-1);
                 }
             }
         }
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
     public static void main(String[] args) {
         int[] arr = {2,3,5,32,223,45,1};
